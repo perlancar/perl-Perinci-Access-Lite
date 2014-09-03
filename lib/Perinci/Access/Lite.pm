ec9462bc2046c366db8190d015219935bc6907cc
perlancar (@pc-office) <perlancar@gmail.com>
2014-09-03 17:33:03 +0700

    Move another code from pericmd-lite (implement call action on pl/schemeless)

--- lib/Perinci/Access/Lite.pm
+++ lib/Perinci/Access/Lite.pm
@@ -29,13 +29,15 @@ sub __package_exists {
 sub request {
     my ($self, $action, $url, $extra) = @_;
 
+    $extra //= {};
+
     if ($url =~ m!\A(?:pl:)?/(\w+(?:/\w+)*)/(\w*)\z!) {
         my ($mod, $func) = ($1, $2);
         # skip if package already exists, e.g. 'main'
         require "$mod.pm" unless __package_exists($mod);
         $mod =~ s!/!::!g;
 
-        if ($action eq 'meta') {
+        if ($action eq 'meta' || $action eq 'call') {
             my $meta;
             {
                 no strict 'refs';
@@ -51,14 +53,50 @@ sub request {
 
             require Perinci::Sub::Normalize;
             $meta = Perinci::Sub::Normalize::normalize_function_metadata($meta);
-            return [200, "OK", $meta];
+            return [200, "OK", $meta] if $action eq 'meta';
+
+            # convert args
+            my $args = $extra->{args} // {};
+            my $aa = $meta->{args_as} // 'hash';
+            my @args;
+            if ($aa =~ /array/) {
+                require Perinci::Sub::ConvertArgs::Array;
+                my $convres = Perinci::Sub::ConvertArgs::Array::convert_args_to_array(
+                    args => $args, meta => $meta,
+                );
+                return $convres unless $convres->[0] == 200;
+                if ($aa =~ /ref/) {
+                    @args = ($convres->[2]);
+                } else {
+                    @args = @{ $convres->[2] };
+                }
+            } elsif ($aa eq 'hashref') {
+                @args = ({ %$args });
+            } else {
+                # hash
+                @args = %$args;
+            }
+
+            # call!
+            my $res;
+            {
+                no strict 'refs';
+                $res = &{"$mod\::$func"}(@args);
+            }
+
+            # add envelope
+            if ($meta->{result_naked}) {
+                $res = [200, "OK (envelope added by ".__PACKAGE__.")", $res];
+            }
+            return $res;
+
         } else {
             return [502, "Unknown/unsupported action '$action'"];
         }
     } elsif (0 && $url =~ m!\Ahttps?:/(/?)!i) {
         my $is_unix = !$1;
     } else {
-        return [502, "Unsupported url scheme or bad URL '$url'"];
+        return [502, "Unsupported scheme or bad URL '$url'"];
     }
 }
 
@@ -72,7 +110,7 @@ prerequisites but does fewer things. Differences with Perinci::Access:
 
 =over
 
-=item * No wrapping
+=item * No wrapping, no argument checking
 
 For 'pl' or schemeless URL, no wrapping (L<Perinci::Sub::Wrapper>) is done, only
 normalization (using L<Perinci::Sub::Normalize>).
